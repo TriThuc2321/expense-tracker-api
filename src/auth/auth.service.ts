2659f0ea5d621992d5e1d126f6ccdd1e57a1f12c
TriThuc2321 <19522321@gm.uit.edu.vn>
2023-01-02 20:13:49 +0700

    init project

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+@Injectable({})
+export class AuthService {
+    register() {
+        return {
+            message: 'Register',
+        };
+    }
+    login() {
+        return {
+            message: 'Login',
+        };
+    }
+}
