f51af9806decf007ddc5feb9aff96e21d2ac8424
TriThuc2321 <19522321@gm.uit.edu.vn>
2023-01-22 20:32:29 +0700

    feat: register

--- src/user/controller.js
+++ src/user/controller.js
@@ -4,8 +4,7 @@ const { getAllQuery, getByIdQuery, checkEmailExistQuery, addUserQuery } = requir
 const getAll = (req, res) => {
     pool.query(getAllQuery, (err, result) => {
         if (err) res.status(400).json(err);
-
-        if (result) {
+        else if (result) {
             res.status(200).json(result.rows);
         } else return null;
     });
@@ -15,25 +14,25 @@ const getById = (req, res) => {
     const id = req.params.id;
     pool.query(getByIdQuery, [id], (err, result) => {
         if (err) res.status(400).json(err);
-        res.status(200).json(result.rows);
+        else res.status(200).json(result.rows);
     });
 };
 
 const addUser = (req, res) => {
-    // console.log(req.body);
-    const name = req.body.name ? req.body.name : 'User';
+    const userId = req.body.userId;
+    const fullName = req.body.fullName ? req.body.fullName : 'User';
     const email = req.body.email ? req.body.email : 'User';
-    const avatar = req.body.avatar
-        ? req.body.avatar
+    const picture = req.body.picture
+        ? req.body.picture
         : 'https://st3.depositphotos.com/1767687/16607/v/450/depositphotos_166074422-stock-illustration-default-avatar-profile-icon-grey.jpg';
 
     pool.query(checkEmailExistQuery, [email], (err, result) => {
         if (err) res.status(400).json(err);
-        if (result.rows.length) res.send('Email already exists.');
+        else if (result.rows.length) res.send('Email already exists.');
         else {
-            pool.query(addUserQuery, ['1', name, email, avatar], (error, result) => {
+            pool.query(addUserQuery, [userId, fullName, email, picture], (error, result) => {
                 if (error) res.status(400).json(error);
-                res.status(200).send('Create account successfully!');
+                else res.status(200).send('Create account successfully!');
             });
         }
     });
